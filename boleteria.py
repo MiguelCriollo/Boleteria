53841ad7755e221d5ecd2b57a0411ec7f6aea830
Miguell Criollo <macriollo.2@sudamericano.edu.ec>
2023-07-25 12:11:14 -0500

    asdasas

--- boleteria.py
+++ boleteria.py
@@ -1,14 +1,20 @@
 import random
+
+animales = ['Ballena', 'Tigre', 'Anaconda', 'Panda', 'Tortuga']
+
 def randomTicket():
-    return [random.randint(0,) for _ in range(4) ]
+    return [random.randint(0,1) for _ in range(2)]
 
+def randomAnimal():
+    return animales[random.randint(0,len(animales)-1)]
 
+winnerTicket=randomTicket()
+winnerAnimal=randomAnimal()
 while True:
-    winnerTicket=randomTicket()
-    input("Enter para comprar ticket")
     ticketUser=randomTicket()
-    print (f"Numero ganador es: {winnerTicket}, tu ticket es: {ticketUser}")
-    if(winnerTicket==ticketUser):
+    boleto = input("Si logras adivinar el animal ganador, puedes volver intentar ganar otros premios con nuevo boleto ")
+    print (f"Numero ganador es: {winnerTicket} y el animal ganador es:{winnerAnimal}, tu ticket es: {ticketUser}")
+    if(winnerTicket==ticketUser and boleto == winnerAnimal):
         print("Ganaste")
         break
     else:
