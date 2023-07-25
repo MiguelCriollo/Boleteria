cde54ae17d1feb6c0f88b18063e71009b0df2727
Miguell Criollo <macriollo.2@sudamericano.edu.ec>
2023-07-25 11:32:20 -0500

    Ticket Genenration

--- boleteria.py
+++ boleteria.py
@@ -1,1 +1,7 @@
-print("Hello World")
+import random
+def randomTicket():
+    ticket=[]
+    for num in range(4):
+        ticket.append(random.randint(0,9))
+    return ticket
+print(randomTicket())
