2b7bee0e39a399246311a4433b4009aef0cd2f03
Kevin-av <116099021+Kevin-av@users.noreply.github.com>
2023-07-25 11:45:14 -0500

    asdas

--- boleteria.py
+++ boleteria.py
@@ -1,1 +1,10 @@
 print("Hello World")
+
+boleto = input("Si logras adivinar el animal ganador, puedes volver intentar ganar otros premios con nuevo boleto ")
+animales = ['Ballena', 'Tigre', 'Anaconda', 'Panda', 'Tortuga']
+resultado = ['Perdedor', 'Perdedor', 'Ganador', 'Perdedor', 'Ganador']
+
+if boleto in [animales[i] for i in range(len(animales)) if resultado[i] == 'Ganador']:
+    print("Felicitaciones, has adivinado correctamente y obtienes otro boleto!")
+else:
+    print("Lo siento, no has adivinado correctamente.")
